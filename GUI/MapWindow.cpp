d8dba38b584f16468049b776ea8fa7c83f0f1a88
bboyles737 <bboyles737@gmail.com>
2017-02-14 15:25:35 -0500

    Update 5: Grid labels and bugfix
    - Grids are now labeled by their position
    - Scale of grids changes as you zoom in and out
    - Fixed bug; previous zooming in and out did not recenter properly

--- GUI/MapWindow.cpp
+++ GUI/MapWindow.cpp
@@ -1,6 +1,8 @@
 #include "MapWindow.h"
+#include "Helpers.h"
 #include "GL/freeglut.h"
 #include <stdio.h>
+#include <sstream>
 
 MapWindow::MapWindow(int sizeX, int sizeY)
 {
@@ -10,7 +12,7 @@ MapWindow::MapWindow(int sizeX, int sizeY)
 
     this->mapRatio = (double)sizeX / sizeY;
     this->mapZoom = 100;
-    this->gridSpacing = 50;
+    this->gridSpacing = 25;
     this->showGridLabels = true;
     this->mapLLH = Vec2(0, 0);
     this->mapWorldDim = Vec2(this->mapZoom * this->mapRatio, this->mapZoom);
@@ -48,21 +50,35 @@ void MapWindow::zoomTo(int newZoom) {
 void MapWindow::recalcMapSize() {
     this->mapWorldDim.x = this->mapZoom * this->mapRatio;
     this->mapWorldDim.y = this->mapZoom;
+    printf("New world dimensions: %d, %d\n", this->mapWorldDim.x, this->mapWorldDim.y);
     this->mapLLH.x = this->mapCentX() - (this->mapWorldDim.x / 2);
     this->mapLLH.y = this->mapCentY() - (this->mapWorldDim.y / 2);
+
+    this->gridSpacing = this->mapZoom / 4;
 }
 
 void MapWindow::drawGridlines(int spacing) {
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINES);
-        for (int x = this->mapLeft() / spacing; x <= this->mapRight() / spacing; x++) {
+    glColor3f(1.0, 1.0, 0.0);
+    std::ostringstream txt;
+
+    for (int x = this->mapLeft() / spacing; x <= this->mapRight() / spacing; x++) {
+        glBegin(GL_LINES);
             glVertex2i(x*spacing, this->mapTop());
             glVertex2i(x*spacing, this->mapBot());
-        }
-        for (int y = this->mapBot() / spacing; y <= this->mapTop() / spacing; y++) {
+        glEnd();
+        txt << (x*spacing);
+        drawStringBitmap(x*spacing + 0.02*this->mapZoom, this->mapBot() + 0.02*this->mapZoom, txt.str());
+        txt.str("");
+    }
+    for (int y = this->mapBot() / spacing; y <= this->mapTop() / spacing; y++) {
+        glBegin(GL_LINES);
             glVertex2i(this->mapLeft(), y*spacing);
             glVertex2i(this->mapRight(), y*spacing);
-        }
+        glEnd();
+        txt << (y*spacing);
+        drawStringBitmap(this->mapLeft() + 0.02*this->mapZoom, y*spacing + 0.02*this->mapZoom, txt.str());
+        txt.str("");
+    }
     glEnd();
 }
 
@@ -86,7 +102,7 @@ void MapWindow::render() {
 
     glColor3f(0.0, 0.0, 0.0);
     glRecti(this->mapLeft(), this->mapBot(), this->mapRight(), this->mapTop());
-    drawGridlines(50);
+    drawGridlines(this->gridSpacing);
 }
 
 Vec2 MapWindow::scrToMap(int mouseX, int mouseY) {
@@ -117,13 +133,16 @@ void MapWindow::mouseHover(int mouseX, int mouseY) {
 void MapWindow::mouseClick(int button, int state, int mouseX, int mouseY) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
         if (!this->centerMode) {
-            this->centerMapOn(this->scrToMap(mouseX, mouseY));
+            Vec2 mouseAt = this->scrToMap(mouseX, mouseY);
             this->zoomIn();
+            this->centerMapOn(mouseAt);
         }
     }
     else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
-        this->centerMapOn(this->scrToMap(mouseX, mouseY));
-        if (!this->centerMode) this->zoomOut();     // Also centers
+        Vec2 mouseAt = this->scrToMap(mouseX, mouseY);
+        if (!this->centerMode) this->zoomOut();
+        this->centerMapOn(mouseAt);
+
     }
 
 }
